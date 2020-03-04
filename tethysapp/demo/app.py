06cb9b9b5fc4a7e17b22ed4429bbed12bc65b7ab
Abhishek Amalaraj <abhishekamalaraj@EB406-MAC09.local>
2020-03-04 14:29:17 -0700

    minor changes

--- tethysapp/demo/app.py
+++ tethysapp/demo/app.py
@@ -11,24 +11,40 @@ class Demo(TethysAppBase):
     icon = 'demo/images/icon.gif'
     package = 'demo'
     root_url = 'demo'
-    color = '#2980b9'
+    color = '#000000'
+    background_color = '#FFFF00'
     description = 'About me app'
     tags = 'Aboutme Intro'
     enable_feedback = False
     feedback_emails = []
 
-    def url_maps(self):
-        """
-        Add controllers
-        """
-        UrlMap = url_map_maker(self.root_url)
+       def url_maps(self):
+           """
+           Add controllers
+           """
+           UrlMap = url_map_maker(self.root_url)
 
-        url_maps = (
-            UrlMap(
-                name='home',
-                url='demo',
-                controller='demo.controllers.home'
-            ),
-        )
+           url_maps = (
+               UrlMap(
+                   name='home',
+                   url='demo',
+                   controller='demo.controllers.home'
+               ),
+               UrlMap(
+                   name='map',
+                   url='demo/map',
+                   controller='demo.controllers.map'
+               ),
+               UrlMap(
+                   name='design',
+                   url='demo/design',
+                   controller='demo.controllers.design_pages'
+               ),
+               UrlMap(
+                   name='proposal',
+                   url='demo/proposal',
+                   controller='demo.controllers.proposal'
+               ))
 
-        return url_maps
+           return url_maps
+
